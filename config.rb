5896e1b8bbcf0379e5457e1814067cd2b7a8ee73
Teijo Laine <teijo@aropupu.fi>
2013-03-30 00:17:32 +0200

    Do not compress yet in Compass phase

--- config.rb
+++ config.rb
@@ -8,17 +8,17 @@ images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
- output_style = :compressed
+# output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+line_comments = true
 
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
